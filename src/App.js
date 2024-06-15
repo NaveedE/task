443bb0d02d50a3760a5f622fe97de2938c02f151
NaveedE <naveededayadan@gmail.com>
2024-06-15 23:53:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./assets/styles.css";
+
+import { useState } from "react";
+
+
+import Tasks from "./components/Tasks";
+import TaskEdit from "./components/TaskEdit";
 
 function App() {
+  const [tasks, setTasks] = useState([
+  
+  ]);
+
+  const onTglStatus = (task) => {
+    console.log("completing task");
+    setTasks(
+      tasks.map((chkTask) => {
+        chkTask.complete =
+          task.id === chkTask.id ? !chkTask.complete : chkTask.complete;
+        return chkTask;
+      })
+    );
+  };
+
+  const [showTaskEdit, setShowTaskEdit] = useState(false);
+
+  const onSaveTask = ({ desc, date }) => {
+    console.log("saving tasks");
+    setTasks([
+      { desc: desc, date: date, id: Date.now(), complete: false },
+      ...tasks,
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+      <div className="container">
+        <div className="col-12 text-right">
+          <button
+            className="button outline"
+            onClick={() => setShowTaskEdit(!showTaskEdit)}>
+            {!showTaskEdit && "New"}
+            {showTaskEdit && "➖"}
+          </button>
+        </div>
+        {showTaskEdit && <TaskEdit task={{}} onSaveTask={onSaveTask} />}
+        <Tasks tasks={tasks} onTglStatus={onTglStatus}></Tasks>
+      </div>
     </div>
   );
 }
